85eace2f39615407365c50e9c4968c681f0a7596
Ahnaf Shahriar Abir <abirahnafshahriar@gmail.com>
2026-09-12 12:33:44 +0600

    State Management, Button Toggle Functionality & Conditional Rendering

--- src/components/players/players.tsx
+++ src/components/players/players.tsx
@@ -1,6 +1,7 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import type { playerType } from '../../types/playerType';
 import AvailablePlayers from './AvailablePlayers';
+import SelectedPlayers from './SelectedPlayers';
 
 interface PlayersProps {
     playersPromise: Promise<playerType[]>
@@ -8,29 +9,36 @@ interface PlayersProps {
 
 const players = ({ playersPromise }: PlayersProps) => {
     const players = use(playersPromise);
-    console.log(players, 'players');
+
+    const [buttonType, setButtonType] = useState("available")
     
-    
+    // const handleUpdateBtnType = (type: "available" | "selected") => {
+    //     setButtonType(type);
+    // }
+
     return (
-        <div className="container mx-auto">
+      <div className="container mx-auto">
+        <div className="flex justify-between gap-4 mb-2">
+          <h2 className="font-bold text xl:">{buttonType === "available" ? "Available Players" : "Selected Players"}</h2>
 
-
-            <div className="flex justify-between gap-4 mb-2">
-                <h2 className="font-bold text xl:">Available Players</h2>
-
-                <div>
-                    <button className="btn btn-success">Available</button>
-                    <button className="btn">Selected</button>
-                </div>
-            </div>
-
-
-
-
-
-            <AvailablePlayers players={players} />
-
+          <div>
+            <button
+              onClick={() => setButtonType("available")}
+              className={`btn ${buttonType === "available" ? "btn-success" : ""} rounded-r-none`}
+            >
+              Available
+            </button>
+            <button
+              onClick={() => setButtonType("selected")}
+              className={`btn ${buttonType === "selected" ? "btn-success" : ""} rounded-l-none`}
+            >
+              Selected
+            </button>
+          </div>
         </div>
+
+        {buttonType === "available" ? (<AvailablePlayers players={players} />) : (<SelectedPlayers></SelectedPlayers>)}
+      </div>
     );
 };
 
